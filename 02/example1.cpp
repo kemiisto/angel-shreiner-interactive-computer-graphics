51fbe2ba632ab4bbc7db04e3e379ee4d6923308b
kemiisto <kemiisto@gmail.com>
2022-11-26 11:45:58 +0300

    feat: add example ch02-example1

---
+++ 02/example1.cpp
@@ -0,0 +1,76 @@
+#include "../main.h"
+#include <tinygl/tinygl.h>
+#include <glm/vec2.hpp>
+#include <random>
+
+constexpr int numPoints = 5000;
+
+class Window final : public tinygl::Window
+{
+public:
+    using tinygl::Window::Window;
+    void init() override;
+    void processInput() override;
+    void draw() override;
+private:
+    tinygl::ShaderProgram program;
+    tinygl::Buffer vbo{tinygl::Buffer::Type::VertexBuffer, tinygl::Buffer::UsagePattern::StaticDraw};
+    tinygl::VertexArrayObject vao;
+};
+
+void Window::init()
+{
+    glm::vec2 positions[numPoints];
+
+    // First, initialize the corners of our gasket with three positions.
+    glm::vec2 vertices[] = {
+        { -1.0f, -1.0f },
+        {  0.0f,  1.0f },
+        {  1.0f, -1.0f }
+    };
+
+    std::random_device device;
+    std::mt19937 engine(device());
+    std::uniform_int_distribution<int> distribution(0, 2);
+
+    // Specify a starting positions for our iterations - it must lie inside any set of three vertices
+    auto u = vertices[0] + vertices[1];
+    auto v = vertices[0] + vertices[2];
+    positions[0] = 0.25f * (u + v);
+
+    // Compute new positions
+    // Each new point is located midway between last point and a randomly chosen vertex
+    for (int i = 0; i < numPoints - 1; ++i) {
+        auto j = distribution(engine);
+        positions[i + 1] = 0.5f * (positions[i] + vertices[j]);
+    }
+
+    vao.bind();
+    vbo.bind();
+    vbo.fill(positions, sizeof(positions));
+
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Vertex, "vshader21.glsl");
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Fragment, "fshader21.glsl");
+    program.link();
+    program.use();
+
+    vao.setAttributeArray(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    vao.enableAttributeArray(0);
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+void Window::processInput()
+{
+    if (getKey(tinygl::Key::Escape) == tinygl::KeyState::Press) {
+        setShouldClose(true);
+    }
+}
+
+void Window::draw()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawArrays(GL_POINTS, 0, numPoints);
+}
+
+MAIN
