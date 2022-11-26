04fabb98221178b15805d5746bdbccf1255ad5ff
kemiisto <kemiisto@gmail.com>
2022-11-26 18:50:08 +0300

    feat: add example ch02-gasket2

---
+++ 02/gasket2.cpp
@@ -0,0 +1,91 @@
+#include "../main.h"
+#include <tinygl/tinygl.h>
+#include <glm/vec2.hpp>
+
+constexpr int numTimesToSubdivide = 5;
+
+std::vector<glm::vec2> positions;
+
+void triangle(const glm::vec2& a, const glm::vec2& b, const glm::vec2& c)
+{
+    positions.push_back(a);
+    positions.push_back(b);
+    positions.push_back(c);
+}
+
+void divideTriangle(const glm::vec2& a, const glm::vec2& b, const glm::vec2& c, int count)
+{
+    // check for end of recursion
+    if (count == 0) {
+        triangle(a, b, c);
+    } else {
+        //bisect the sides
+        auto ab = 0.5f * (a + b);
+        auto ac = 0.5f * (a + c);
+        auto bc = 0.5f * (b + c);
+        --count;
+        // three new triangles
+        divideTriangle(a, ab, ac, count);
+        divideTriangle(c, ac, bc, count);
+        divideTriangle(b, bc, ab, count);
+    }
+}
+
+class Window final : public tinygl::Window
+{
+public:
+    using tinygl::Window::Window;
+    void init() override;
+    void processInput() override;
+    void draw() override;
+private:
+    tinygl::ShaderProgram program;
+    tinygl::Buffer vbo{tinygl::Buffer::Type::VertexBuffer, tinygl::Buffer::UsagePattern::StaticDraw};
+    tinygl::VertexArrayObject vao;
+};
+
+void Window::init()
+{
+    // First, initialize the corners of our gasket with three positions.
+    glm::vec2 vertices[] = {
+        { -1.0f, -1.0f },
+        {  0.0f,  1.0f },
+        {  1.0f, -1.0f }
+    };
+
+    divideTriangle(vertices[0], vertices[1], vertices[2], numTimesToSubdivide);
+
+    // Configure OpenGL
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    // Load shaders and initialize attribute buffers
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Vertex, "vshader21.glsl");
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Fragment, "fshader21.glsl");
+    program.link();
+    program.use();
+
+    // Load the data into the GPU
+    vao.bind();
+    vbo.bind();
+    vbo.fill(positions.begin(), positions.end());
+
+    // Associate shader variables with our data buffer
+    auto vertexPositionLoc = program.attributeLocation("vertexPosition");
+    vao.setAttributeArray(vertexPositionLoc, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    vao.enableAttributeArray(vertexPositionLoc);
+}
+
+void Window::processInput()
+{
+    if (getKey(tinygl::Key::Escape) == tinygl::KeyState::Press) {
+        setShouldClose(true);
+    }
+}
+
+void Window::draw()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawArrays(GL_TRIANGLES, 0, positions.size());
+}
+
+MAIN
