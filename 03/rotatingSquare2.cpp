3057332ea1cf962f73c5be536c18d17c103eb7de
kemiisto <kemiisto@gmail.com>
2022-11-27 12:24:25 +0300

    feat: add example ch03-rotatingSquare2

---
+++ 03/rotatingSquare2.cpp
@@ -0,0 +1,70 @@
+#include "../main.h"
+#include <tinygl/tinygl.h>
+#include <glm/vec2.hpp>
+#include <array>
+
+class Window final : public tinygl::Window
+{
+public:
+    using tinygl::Window::Window;
+    void init() override;
+    void processInput() override;
+    void draw() override;
+private:
+    tinygl::ShaderProgram program;
+    tinygl::Buffer vBuffer{tinygl::Buffer::Type::VertexBuffer, tinygl::Buffer::UsagePattern::StaticDraw};
+    tinygl::VertexArrayObject vao;
+
+    float theta = 0.0f;
+    int thetaLoc = -1;
+};
+
+void Window::init()
+{
+    // Configure OpenGL.
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    // Load shaders and initialize attribute buffers.
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Vertex, "rotatingSquare1.vert");
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Fragment, "rotatingSquare1.frag");
+    program.link();
+    program.use();
+
+    std::array vertices = {
+        glm::vec2{ 0.0f,  1.0f},
+        glm::vec2{-1.0f,  0.0f},
+        glm::vec2{ 1.0f,  0.0f},
+        glm::vec2{ 0.0f, -1.0f}
+    };
+
+    // Load the data into the GPU
+    vao.bind();
+    vBuffer.bind();
+    vBuffer.fill(vertices.begin(), vertices.end());
+
+    // Associate shader variables with our data buffer
+    auto positionLoc = program.attributeLocation("aPosition");
+    vao.setAttributeArray(positionLoc, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    vao.enableAttributeArray(positionLoc);
+
+    thetaLoc = program.uniformLocation("uTheta");
+}
+
+void Window::processInput()
+{
+    if (getKey(tinygl::Key::Escape) == tinygl::KeyState::Press) {
+        setShouldClose(true);
+    }
+}
+
+void Window::draw()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    theta += 0.1f;
+    program.setUniformValue(thetaLoc, theta);
+
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+}
+
+MAIN
